694ded8cbd0f9be68baaeea202700b7410ea8670
ShinjiMC <nayapbrau@gmail.com>
2024-03-15 23:51:16 -0500

    Add Models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,20 +1,28 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Booking(models.Model):
-    first_name = models.CharField(max_length=200)
-    reservation_date = models.DateField()
-    reservation_slot = models.SmallIntegerField(default=10)
+    name = models.CharField(max_length=255, default='')
+    number_of_guests = models.IntegerField(default=6)
+    booking_date =models.DateTimeField(default=timezone.now)
 
-    def __str__(self): 
-        return self.first_name
+    class Meta:
+        verbose_name = 'Booking'
+        verbose_name_plural = 'Booking Records'
+
+    def __str__(self) -> str:
+        return f'{self.name} for {self.number_of_guests} guests on {self.booking_date}'
 
 
 # Add code to create Menu model
 class Menu(models.Model):
-   name = models.CharField(max_length=200) 
-   price = models.IntegerField(null=False) 
-   menu_item_description = models.TextField(max_length=1000, default='') 
+    title = models.CharField(max_length=255, default='')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    inventory = models.IntegerField(default=5)
 
-   def __str__(self):
-      return self.name
+    class Meta:
+        verbose_name = 'Menu'
+        verbose_name_plural = 'Menu Items'
+
+    def __str__(self) -> str:
+        return f'{self.title} : {str(self.price)}'
